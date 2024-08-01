23d74b50fc3bfaae75ec96f315c8c02f3bcdebf0
smtdeveloper <sametakcatoki@gmail.com>
2024-08-01 13:41:35 +0300

    Core paket eklendi

--- Domain/Entities/FoodPackage.cs
+++ Domain/Entities/FoodPackage.cs
@@ -1,18 +1,30 @@
-﻿namespace Domain.Entities;
+﻿using Core.Persistence.Repositories;
 
-public class FoodPackage
+namespace Domain.Entities;
+
+public class FoodPackage : Entity<Guid>
 {
-    public int PackageId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public decimal OriginalPrice { get; set; }
     public decimal DiscountedPrice { get; set; }
     public DateTime AvailableFrom { get; set; }
     public DateTime AvailableUntil { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
     //public int BusinessId { get; set; }
     //public Business Business { get; set; }
 
     //public ICollection<Order> Orders { get; set; }
+
+    public FoodPackage()
+    {
+        
+    }
+
+    public FoodPackage(Guid id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
 }
