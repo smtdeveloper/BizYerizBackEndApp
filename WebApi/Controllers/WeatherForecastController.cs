b074bdcc8ab796232bcd59159d5b605a435f1a97
Samet Akca <smtakca99@gmail.com>
2024-08-03 19:29:21 +0300

    SmtCoder test

--- WebApi/Controllers/WeatherForecastController.cs
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace WebApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching SmtCoder"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
