12d6f0a182ebd263e9bce06d7ed32b548dca1c36
Samet Akca <smtakca99@gmail.com>
2024-08-07 20:32:48 +0300

    BaseDbContext

---
+++ Application/Services/Repositories/IFoodPackagesRepository.cs
@@ -0,0 +1,12 @@
+﻿using Core.Persistence.Repositories;
+using Domain.Entities;
+
+namespace Application.Services.Repositories;
+
+public interface IFoodPackagesRepository : IAsyncRepository<FoodPackage, Guid>, IRepository<FoodPackage, Guid>
+{
+
+
+
+}
+
---
+++ Persistence/EntityConfigurations/FoodPackageConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.EntityConfigurations
+{
+    public class FoodPackageConfiguration : IEntityTypeConfiguration<FoodPackage>
+    {
+        
+        public void Configure(EntityTypeBuilder<FoodPackage> builder)
+        {
+            builder.ToTable("FoodPackage");
+
+        }
+
+
+    }
+}
