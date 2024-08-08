b4cccba4f3988bc694c72f87dffe970e29f228eb
Samet Akca <smtakca99@gmail.com>
2024-08-08 16:53:52 +0300

    FoodPackageConfiguration

--- Persistence/EntityConfigurations/FoodPackageConfiguration.cs
+++ Persistence/EntityConfigurations/FoodPackageConfiguration.cs
@@ -9,9 +9,15 @@ namespace Persistence.EntityConfigurations
         
         public void Configure(EntityTypeBuilder<FoodPackage> builder)
         {
-            builder.ToTable("FoodPackage");
-
+            builder.ToTable("FoodPackage").HasKey(f => f.Id);
+            builder.Property(f => f.Id).HasColumnName("Id").IsRequired();
+            builder.Property(f => f.Name).HasColumnName("Name").IsRequired();
+            builder.Property(f => f.CreatedDate).HasColumnName("CreatedDate").IsRequired();
+            builder.Property(f => f.UpdatedDate).HasColumnName("UpdatedDate");
+            builder.Property(f => f.DeletedDate).HasColumnName("DeletedDate");
+            
         }
+
 
 
     }
